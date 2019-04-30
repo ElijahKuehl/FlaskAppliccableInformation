29e64696b90de5d832e9fa5cf0771b623b23698d
Elijah Kuehl <kuehl.elijah.b@gmail.com>
2019-04-30 21:41:45 +0000

    initial changes

--- static/js/home.js
+++ static/js/home.js
@@ -35,53 +35,6 @@ $(document).ready(function(){
     $("#search-results").hide();
     $("#resultCount").hide();
 
-    $("#search-button").click(function(event){
-        document.getElementById("bgColour").style.height= "";
-        search(event);
-        $("#search-results").show();
-        $("#gotoBottom").show();
-    });
-
-    $("#team").click(function(event){
-        document.getElementById("team").value="1";
-        document.getElementById("bgColour").style.height= "";
-        search(event);
-        $("#search-results").show();
-        $("#gotoBottom").show();
-    });
-
-    $(".sortBtn").click(function(){
-        $(".sortMenu").hide();
-        $(".sortMenu").val('Default');
-        $(".sortBtn").show();
-    });
-
-    $("#filterClear").click(function(){
-        $(".filterMenu").hide();
-        $(".filterMenu").val('Default');
-        $(".filterBtn").show();
-        $("#Type2Btn").hide();
-    });
-
-    function filterClick(name){
-        $("#"+name+"Btn").click(function(){
-            $("#"+name+"Menu").show();
-            $("#"+name+"Btn").hide();
-        });
-    }
-
-    filterClick("Generation");
-    filterClick("Legendary");
-    filterClick("Type2");
-    filterClick("HighStats");
-    filterClick("LowStats");
-
-    $("#TypeBtn").click(function(){
-        $("#TypeMenu").show();
-        $("#TypeBtn").hide();
-        $("#Type2Btn").show();
-    });
-
 
     $("#artFlip").click(function(){
         if(document.getElementById("artFlip").checked){
