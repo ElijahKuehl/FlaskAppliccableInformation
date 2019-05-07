3c75ebdd5eaa3c92ca3e33ec587db658aae49dfe
Elijah Kuehl <kuehl.elijah.b@gmail.com>
2019-05-07 21:40:12 +0000

    added all the version 2 files

--- flask_app.py
+++ flask_app.py
@@ -14,7 +14,6 @@ engine = create_engine(Pokedex)
 def index():
     return render_template('main_page.html')
 
-
 @app.route('/pokedex', methods=['Get'])
 def process_inputs():
     pokemon = request.args["pokeName"]
@@ -23,8 +22,6 @@ def process_inputs():
     generationFilt = request.args["generationFilt"]
     legendFilt = request.args["legendFilt"]
     typeFilt = request.args["typeFilt"]
-    type2Filt = request.args["type2Filt"]
-    randTeam = request.args["randTeam"]
     nameFilt = ""
 
     if ' -' in pokemon:
@@ -36,46 +33,113 @@ def process_inputs():
             val += 1
         pokemon = pokemon.split(' -')[0]
 
-
-    #Sort by Stats, highest first, if a random team wasnt requested.
-    if randTeam == "1":
-        sort = "ORDER BY RAND() LIMIT 6"
-    elif highStats != "Default" and lowStats == "Default":
-        sort = "ORDER BY "+ highStats +" DESC"
+    if False == True:
+        sort = ""
+    #Sort by Stats, highest first.
+    elif highStats == "HighTotal":
+        sort = "ORDER BY total DESC"
+    elif highStats == "HighHp":
+        sort = "ORDER BY hp DESC"
+    elif highStats == "HighAtk":
+        sort = "ORDER BY attack DESC"
+    elif highStats == "HighDef":
+        sort = "ORDER BY defense DESC"
+    elif highStats == "HighSAtk":
+        sort = "ORDER BY sp_attack DESC"
+    elif highStats == "HighSDef":
+        sort = "ORDER BY sp_defense DESC"
+    elif highStats == "HighSpd":
+        sort = "ORDER BY Speed DESC"
     #Sort by Stats, lowest first.
-    elif lowStats != "Default" and highStats == "Default":
-        sort = "ORDER BY "+ lowStats +" ASC"
+    elif lowStats == "LowTotal":
+        sort = "ORDER BY total ASC"
+    elif lowStats == "LowHp":
+        sort = "ORDER BY hp ASC"
+    elif lowStats == "LowAtk":
+        sort = "ORDER BY attack ASC"
+    elif lowStats == "LowDef":
+        sort = "ORDER BY defense ASC"
+    elif lowStats == "LowSAtk":
+        sort = "ORDER BY sp_attack ASC"
+    elif lowStats == "LowSDef":
+        sort = "ORDER BY sp_defense ASC"
+    elif lowStats == "LowSpd":
+        sort = "ORDER BY Speed ASC"
     #Don't Sort
     else:
         sort = ""
 
     #Filters a certian Generation
-    if generationFilt != "Default":
-        genFilt = "AND generation="+ generationFilt +" "
+    if generationFilt == "1":
+        genFilt = "AND generation=1 "
+    elif generationFilt == "2":
+        genFilt = "AND generation=2 "
+    elif generationFilt == "3":
+        genFilt = "AND generation=3 "
+    elif generationFilt == "4":
+        genFilt = "AND generation=4 "
+    elif generationFilt == "5":
+        genFilt = "AND generation=5 "
+    elif generationFilt == "6":
+        genFilt = "AND generation=6 "
+    elif generationFilt == "7":
+        genFilt = "AND generation=7 "
+    elif generationFilt == "8":
+        genFilt = "AND generation=8 "
     #Don't filter Generation
     else:
         genFilt = ""
 
     #Filters if a pokemon is Legendary/Mythical or not.
-    if legendFilt != "Default":
-        legFilt = "AND legendary='"+legendFilt+"' "
+    if legendFilt == "Is":
+        legFilt = "AND legendary='True' "
+    elif legendFilt == "Not":
+        legFilt = "AND legendary='False' "
     #Don't filter Legendary
     else:
         legFilt = ""
 
+#TODO: Make a second Type menu.
     #Filters based on a certian type.
-    def types(var):
-        if var == "None":
-            return "AND (type1='' OR type2='') "
-        elif var != "Default":
-            return "AND (type1='"+var+"' OR type2='"+var+"') "
-        #Don't filter Type
-        else:
-            return ""
+    if typeFilt == "Bug":
+        typFilt = "AND (type1='Bug' OR type2='Bug') "
+    elif typeFilt == "Dragon":
+        typFilt = "AND (type1='Dragon' OR type2='Dragon') "
+    elif typeFilt == "Ice":
+        typFilt = "AND (type1='Ice' OR type2='Ice') "
+    elif typeFilt == "Fighting":
+        typFilt = "AND (type1='Fighting' OR type2='Fighting') "
+    elif typeFilt == "Fire":
+        typFilt = "AND (type1='Fire' OR type2='Fire') "
+    elif typeFilt == "Flying":
+        typFilt = "AND (type1='Flying' OR type2='Flying') "
+    elif typeFilt == "Grass":
+        typFilt = "AND (type1='Grass' OR type2='Grass') "
+    elif typeFilt == "Ghost":
+        typFilt = "AND (type1='Ghost' OR type2='Ghost') "
+    elif typeFilt == "Ground":
+        typFilt = "AND (type1='Ground' OR type2='Ground') "
+    elif typeFilt == "Electric":
+        typFilt = "AND (type1='Electric' OR type2='Electric') "
+    elif typeFilt == "Normal":
+        typFilt = "AND (type1='Normal' OR type2='Normal') "
+    elif typeFilt == "Posion":
+        typFilt = "AND (type1='Posion' OR type2='Posion') "
+    elif typeFilt == "Psychic":
+        typFilt = "AND (type1='Psychic' OR type2='Psychic') "
+    elif typeFilt == "Rock":
+        typFilt = "AND (type1='Rock' OR type2='Rock') "
+    elif typeFilt == "Water":
+        typFilt = "AND (type1='Water' OR type2='Water') "
+    elif typeFilt == "Dark":
+        typFilt = "AND (type1='Dark' OR type2='Dark') "
+    elif typeFilt == "Steel":
+        typFilt = "AND (type1='Steel' OR type2='Steel') "
+    elif typeFilt == "Fairy":
+        typFilt = "AND (type1='Fairy' OR type2='Fairy') "
+    #Don't filter Type
+    else:
+        typFilt = ""
 
-    typFilt = types(typeFilt)
-    typ2Filt = types(type2Filt)
-
-    results = engine.execute(("SELECT id, number, name, type1, CASE type2 WHEN '' THEN 'None' ELSE type2 END AS type2, total, hp, attack, defense, sp_attack, sp_defense, speed, generation, CASE legendary WHEN 'False' THEN 'Not' WHEN 'True' THEN 'Is' END AS legendary FROM Pokedex WHERE name LIKE '%%{}%%' "+nameFilt+genFilt+legFilt+typFilt+typ2Filt+sort+";").format(pokemon))
-
+    results = engine.execute(("SELECT id, number, name, type1, CASE type2 WHEN '' THEN 'None' ELSE type2 END AS type2, total, hp, attack, defense, sp_attack, sp_defense, speed, generation, CASE legendary WHEN 'False' THEN 'Not' WHEN 'True' THEN 'Is' END AS legendary FROM Pokedex WHERE name LIKE '%%{}%%' "+nameFilt+genFilt+legFilt+typFilt+sort+";").format(pokemon))
     return jsonify([(dict(row.items())) for row in results])
--- static/js/home.js
+++ static/js/home.js
@@ -13,42 +13,25 @@ $(document).ready(function(){
         $(".Generation").hide();
         $(".Legendary").hide();
         $(".Type").hide();
-        $("#Type2Btn").hide();
         $("#gotoBottom").hide();
         checkArtJS("victini");
-        console.log($(window).width());
-        document.getElementById("weaknesses").style.display = "none";
-        document.getElementById("weaknesses").style.top = 30 + "px";
-        document.getElementById("weaknesses").style.left = 88 + "%";
         document.getElementById("images").style.top = 280 + "px";
         document.getElementById("images").style.left = 30 + "px";
         document.getElementById("pkmnImage").name = "victini";
-        document.getElementById("bgColour").style.height= "100%";
         });
 
-    document.getElementById("weaknesses").style.display = "none";
     $(".sortMenu").hide();
     $(".sortBtn").show();
     $(".filterMenu").hide();
     $(".filterBtn").show();
-    $("#Type2Btn").hide();
     $("#search-results").hide();
     $("#resultCount").hide();
 
-    $("#search-button").click(function(event){
-        document.getElementById("bgColour").style.height= "";
-        search(event);
+
+    $("#search-button").click(function(){
         $("#search-results").show();
-        $("#gotoBottom").show();
     });
 
-    $("#team").click(function(event){
-        document.getElementById("team").value="1";
-        document.getElementById("bgColour").style.height= "";
-        search(event);
-        $("#search-results").show();
-        $("#gotoBottom").show();
-    });
 
     $(".sortBtn").click(function(){
         $(".sortMenu").hide();
@@ -56,31 +39,39 @@ $(document).ready(function(){
         $(".sortBtn").show();
     });
 
+    $("#HighStatsBtn").click(function(){
+        $("#HighStatsMenu").show();
+        $("#HighStatsBtn").hide();
+    });
+
+    $("#LowStatsBtn").click(function(){
+        $("#LowStatsMenu").show();
+        $("#LowStatsBtn").hide();
+    });
+
+
     $("#filterClear").click(function(){
         $(".filterMenu").hide();
         $(".filterMenu").val('Default');
         $(".filterBtn").show();
-        $("#Type2Btn").hide();
     });
 
-    function filterClick(name){
-        $("#"+name+"Btn").click(function(){
-            $("#"+name+"Menu").show();
-            $("#"+name+"Btn").hide();
-        });
-    }
+    $("#GenerationBtn").click(function(){
+        $("#GenerationMenu").show();
+        $("#GenerationBtn").hide();
+    });
 
-    filterClick("Generation");
-    filterClick("Legendary");
-    filterClick("Type2");
-    filterClick("HighStats");
-    filterClick("LowStats");
+    $("#LegendaryBtn").click(function(){
+        $("#LegendaryMenu").show();
+        $("#LegendaryBtn").hide();
+    });
 
     $("#TypeBtn").click(function(){
         $("#TypeMenu").show();
         $("#TypeBtn").hide();
-        $("#Type2Btn").show();
     });
+
+    $("#search-button").click(function(event) {search(event);});
 
 
     $("#artFlip").click(function(){
@@ -121,24 +112,25 @@ $(document).ready(function(){
     }
 
 
-    window.onscroll = function() {scrollFunction();};
+    window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-        var scroll = $(window).scrollTop() + $(window).height();
-        var div = $('body');
-        var bottom = ((div.offset().top + div.height()) - scroll);
+    var scroll = $(window).scrollTop() + $(window).height();
+    var div = $('body');
+    var bottom = ((div.offset().top + div.height()) - scroll);
 
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {$("#gotoTop").show();}
-        else {$("#gotoTop").hide();}
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {$("#gotoTop").show();}
+    else {$("#gotoTop").hide();}
 
-        if (bottom > 20) {$("#gotoBottom").show();}
-        else {$("#gotoBottom").hide();}
+    if (bottom > 20) {$("#gotoBottom").show();}
+    else {$("#gotoBottom").hide();}
 
-        if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {$("#copy").show();}
-        else {$("#copy").hide();}
+    if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {$("#copy").show();}
+    else {$("#copy").hide();}
     }
 
 
+    dragElement(document.getElementById("images"));
     var input = document.getElementById("search-bar");
 
     input.addEventListener("keyup", function(event) {
@@ -148,8 +140,7 @@ $(document).ready(function(){
       }
     });
 
-
-    function search(event){
+function search(event){
         event.preventDefault();
         var pokeNames = $("#search-bar").val();
         var highStats = document.getElementById("HighStatsMenu").value;
@@ -157,128 +148,110 @@ $(document).ready(function(){
         var generationFilt = document.getElementById("GenerationMenu").value;
         var legendFilt = document.getElementById("LegendaryMenu").value;
         var typeFilt = document.getElementById("TypeMenu").value;
-        var type2Filt = document.getElementById("Type2Menu").value;
-        var randTeam = document.getElementById("team").value;
         var replaceName = "";
         var imeg = "";
-        //Ill be honest idk why I made this line. if (pokeNames.toLowerCase().replace('primal ', '').replace('\'','').replace(' forme', '').replace(' size', '').replace(' mode', '').replace('black ', '').replace('white ', '').replace(' cloak','').replace('%','')==""){pokeNames="";}
+        //TODO: Make a second Type menu.
+        if (pokeNames.toLowerCase().replace('primal ', '').replace(' ', '-').replace('\'','').replace(' forme', '').replace(' size', '').replace(' mode', '').replace('black-', '').replace('white-', '').replace(' cloak','').replace('%','')==""){
+            pokeNames="";
+        }
         //This gets sent to python
-        var queryString = "/pokedex"
-        + "?pokeName=" + pokeNames
-        + "&highStats=" + highStats
-        + "&lowStats=" + lowStats
-        + "&generationFilt=" + generationFilt
-        + "&legendFilt=" + legendFilt
-        + "&typeFilt=" + typeFilt
-        + "&type2Filt=" + type2Filt
-        + "&randTeam=" + randTeam;
-        document.getElementById("team").value="0";
+        var queryString = "/pokedex" + "?pokeName=" + pokeNames + "&highStats=" + highStats + "&lowStats=" + lowStats + "&generationFilt=" + generationFilt + "&legendFilt=" + legendFilt + "&typeFilt=" + typeFilt;
 
         $.getJSON( queryString, function (json) {
             var items = [];
+
             if (json.length != 0){
                 var s2 = "";
                 if(json.length > 1){s2="s";}
-                if(json.length < 6 && $(window).height()>700){document.getElementById("bgColour").style.height= "100%";} //console.log($(window).height());
                 items.push(json.length + " result"+s2+" found!");
-                $.each(json, function(key, val){
-                    replaceName = this.name.toLowerCase().replace('alolan ','AL').replace('dawn wings ','DW').replace('dusk mane ','DM').replace('ultra ','UU').replace('ash-','AA').replace(' x','XX').replace(' y','YY').replace('mega ', 'MM').replace('primal ', 'PP').replace('black ', 'BB').replace('white ', 'WW')
-                    .replace('. ', '-').replace(' ', '-').replace('\'','').replace(' forme', '').replace(' size', '').replace(' mode', '').replace(' cloak','').replace(' style','').replace('flabébé','flabebe').replace('jr.', 'jr').replace('♂','-m').replace('♀','-f').replace('%','').replace('’','').replace(':','');
-                    if (replaceName.includes('MM')){replaceName = replaceName.replace('MM', '') + "-mega";}
-                    if (replaceName.includes('XX')){replaceName = replaceName.replace('XX', '') + "-x";}
-                    if (replaceName.includes('YY')){replaceName = replaceName.replace('YY', '') + "-y";}
-                    if (replaceName.includes('PP')){replaceName = replaceName.replace('PP', '') + "-primal";}
-                    if (replaceName.includes('BB')){replaceName = replaceName.replace('BB', '') + "-black";}
-                    if (replaceName.includes('WW')){replaceName = replaceName.replace('WW', '') + "-white";}
-                    if (replaceName.includes('AA')){replaceName = replaceName.replace('AA', '') + "-ash";}
-                    if (replaceName.includes('AL')){replaceName = replaceName.replace('AL', '') + "-alolan";}
-                    if (replaceName.includes('DW')){replaceName = replaceName.replace('DW', '') + "-dawn-wings";}
-                    if (replaceName.includes('DM')){replaceName = replaceName.replace('DM', '') + "-dusk-mane";}
-                    if (replaceName.includes('UU')){replaceName = replaceName.replace('UU', '') + "-ultra";}
-                    if (replaceName.includes('-rotom')){replaceName = "rotom-" + replaceName.replace('-rotom', '');}
-                    console.log(replaceName);
+            $.each(json, function(key, val){
+                replaceName = this.name.toLowerCase().replace('alolan ','AL').replace('dawn wings ','DW').replace('dusk mane ','DM').replace('ultra ','UU').replace('ash-','AA').replace(' x','XX').replace(' y','YY').replace('mega ', 'MM').replace('primal ', 'PP').replace('. ', '-').replace('jr.', 'jr').replace(' ', '-').replace('\'','').replace(' forme', '').replace(' size', '').replace(' mode', '').replace('black-', 'BB').replace('white-', 'WW').replace('♂','-m').replace('♀','-f').replace(' cloak','').replace('%','').replace('é','e').replace('é','e').replace(' style','').replace('’','').replace(':','');
+                if (replaceName.includes('MM')){replaceName = replaceName.replace('MM', '') + "-mega";}
+                if (replaceName.includes('XX')){replaceName = replaceName.replace('XX', '') + "-x";}
+                if (replaceName.includes('YY')){replaceName = replaceName.replace('YY', '') + "-y";}
+                if (replaceName.includes('PP')){replaceName = replaceName.replace('PP', '') + "-primal";}
+                if (replaceName.includes('BB')){replaceName = replaceName.replace('BB', '') + "-black";}
+                if (replaceName.includes('WW')){replaceName = replaceName.replace('WW', '') + "-white";}
+                if (replaceName.includes('AA')){replaceName = replaceName.replace('AA', '') + "-ash";}
+                if (replaceName.includes('AL')){replaceName = replaceName.replace('AL', '') + "-alolan";}
+                if (replaceName.includes('DW')){replaceName = replaceName.replace('DW', '') + "-dawn-wings";}
+                if (replaceName.includes('DM')){replaceName = replaceName.replace('DM', '') + "-dusk-mane";}
+                if (replaceName.includes('UU')){replaceName = replaceName.replace('UU', '') + "-ultra";}
+                if (replaceName.includes('-rotom')){replaceName = "rotom-" + replaceName.replace('-rotom', '');}
+                console.log(replaceName);
 
-                    var t1=val.type1;
-                    var t2=val.type2;
-                    items.push(
-                    "<li>" + " | "
-                    + val.number + " | "
-                    + "<button onclick=\"checkArt(this.name); weaknesses('"+val.type1+"', '"+val.type2+"');\" name="+replaceName+" class='pkmnName lead'>" + val.name + "</button>" + " | "
-                    + "Type 1: " + val.type1 + " | "
-                    + "Type 2: " + val.type2 + " | "
-                    + "Generation " + val.generation + " | "
-                    + val.legendary + " Legendary" + " | "
-                    + "<br>" + " | "
-                    + "Stat Total: " + val.total + " | "
-                    + "HP: " + val.hp + " | "
-                    + "Attack: " + val.attack + " | "
-                    + "Defense: " + val.defense + " | "
-                    + "Special Attack: " + val.sp_attack + " | "
-                    + "Special Defense: " + val.sp_defense + " | "
-                    + "Speed: " + val.speed + " | "
-                    + "</li>");
-                });
-            }
-            else{
-                items.push("Whoops! No results! Try something less specific.");
-                document.getElementById("bgColour").style.height= "100%";
-            }
-            console.log(items);
+
+        items.push(
+         "<li>" + " | "
+        + val.number + " | "
+        + "<button onclick=\"checkArt(this.name);\" name="+replaceName+" class='pkmnName lead'>" + val.name + "</button>" + " | "
+        + "Type 1: " + val.type1 + " | "
+        + "Type 2: " + val.type2 + " | "
+        + "Generation " + val.generation + " | "
+        + val.legendary + " Legendary" + " | "
+        + "<br>" + " | "
+        + "Stat Total: " + val.total + " | "
+        + "HP: " + val.hp + " | "
+        + "Attack: " + val.attack + " | "
+        + "Defense: " + val.defense + " | "
+        + "Special Attack: " + val.sp_attack + " | "
+        + "Special Defense: " + val.sp_defense + " | "
+        + "Speed: " + val.speed + " | "
+        + "</li>");
+        });
+        /*var s = "";
+        if(json.length > 1){s="s";}
+        $("#resultCount").show();
+        document.getElementById("resultCount").innerHTML = json.length + " result"+s+" found!";*/
+        }
+        else{items.push("Whoops! No results! Try something less specific.");}
             items.push();
             $("#search-results").html(items);
         });
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    if((elmnt.offsetLeft - pos1)>10&&(elmnt.offsetLeft - pos1)<40&&(elmnt.offsetTop - pos2)>260&&(elmnt.offsetTop - pos2)<300){
+        elmnt.style.top = 280 + "px";
+        elmnt.style.left = 30 + "px";
     }
+  }
 
-
-    dragElement(document.getElementById("images"), "img");
-    dragElement(document.getElementById("weaknesses"), "weak");
-
-    function dragElement(elmnt, name) {
-        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (document.getElementById(elmnt.id + "header")) {
-            /* if present, the header is where you move the DIV from:*/
-            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-        } else {
-            /* otherwise, move the DIV from anywhere inside the DIV:*/
-            elmnt.onmousedown = dragMouseDown;
-        }
-
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-            if(name=="img" && ((elmnt.offsetLeft - pos1)>10&&(elmnt.offsetLeft - pos1)<40&&(elmnt.offsetTop - pos2)>260&&(elmnt.offsetTop - pos2)<300)){
-                elmnt.style.top = 280 + "px";
-                elmnt.style.left = 30 + "px";
-            }
-            if(name=="weak" && ((elmnt.offsetLeft - pos1)>($(window).width()*0.875)&&(elmnt.offsetLeft - pos1)<($(window).width()*0.885)&&(elmnt.offsetTop - pos2)>0&&(elmnt.offsetTop - pos2)<40)){
-                elmnt.style.top = 30 + "px";
-                elmnt.style.left = 88 + "%";
-            }
-        }
-
-        function closeDragElement() {
-            /* stop moving when mouse button is released:*/
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    }
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 });
